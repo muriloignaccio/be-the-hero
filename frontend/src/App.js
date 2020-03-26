0f795acce1d8d779968abaf53f99a8d0bb0910cb
muriloignaccio <murilohsignacio@gmail.com>
2020-03-26 20:21:23 -0300

    finalized application's backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack
+        <p>OmMurilo
         </p>
         <a
           className="App-link"
